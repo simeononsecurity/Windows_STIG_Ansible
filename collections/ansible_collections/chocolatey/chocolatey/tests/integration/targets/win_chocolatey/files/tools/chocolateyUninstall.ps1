6480bb7db324baafbf30f09b15afab797ce44da1
simeononsecurity <4913771+simeononsecurity@users.noreply.github.com>
2022-05-02 15:21:29 -0600

    Remove Dependencies

--- collections/ansible_collections/chocolatey/chocolatey/tests/integration/targets/win_chocolatey/files/tools/chocolateyUninstall.ps1
+++
@@ -1,9 +0,0 @@
-$ErrorActionPreference = 'Stop'
-
-$package_name = $env:ChocolateyPackageName
-$package_version = $env:ChocolateyPackageVersion
-$install_path = "--- PATH ---\$package_name-$package_version.txt"
-
-if (Test-Path -LiteralPath $install_path) {
-    Remove-Item -LiteralPath $install_path -Force > $null
-}
